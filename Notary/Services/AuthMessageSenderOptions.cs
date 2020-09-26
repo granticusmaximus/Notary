1879fc99a8c84b835a30b6f9cbd3fb5b3d606006
Grant Watson <gwatson117@gmail.com>
2020-09-26 16:44:01 -0600

    Addition of files 2

---
+++ Notary/Services/AuthMessageSenderOptions.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace Notary.Services
+{
+    public class AuthMessageSenderOptions
+    {
+        public string SendGridUser { get; set; }
+        public string SendGridKey { get; set; }
+    }
+}
